9ff1988d97f5213f9b83aa85592ae0dee788b07f
szymon.zietek721@gmail.com <szymon.zietek721@gmail.com>
2023-07-10 19:12:12 +0200

    Add step1 in feature 1

--- app_good_hands/goodplan/views.py
+++ app_good_hands/goodplan/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import View
 from django.template.response import TemplateResponse
-from .models import Donation, Institution
+from .models import Donation, Institution, Category
 from django.contrib import messages
 
 
@@ -27,15 +28,45 @@ class LandingPageView(View):
         return render(request, "index.html", context)
 
 
-class AddDonationView(View):
+class AddDonationView(LoginRequiredMixin, View):
     """
     View AddDonation
     """
 
-    @staticmethod
-    def get(request):
-        return TemplateResponse(request, "form.html")
+    login_url = 'login'
 
+    def get(self, request):
+        categories = Category.objects.all()
+        institutions = Institution.objects.all()
+
+        context = {
+            'categories': categories,
+            'institutions': institutions,
+        }
+        return render(request, 'form.html', context=context)
+
+    def post(self, request):
+        quantity = request.POST.get('bags')
+        categories = request.POST.getlist('categories')
+        institution = request.POST.get('organization')
+        address = request.POST.get('address')
+        city = request.POST.get('city')
+        zip_code = request.POST.get('postcode')
+        phone_number = request.POST.get('phone')
+        pick_up_date = request.POST.get('date')
+        pick_up_time = request.POST.get('time')
+        pick_up_comment = request.POST.get('more_info')
+
+
+        user = request.user
+        donation = Donation.objects.create(quantity=quantity, institution_id=int(institution), address=address,
+                                           phone_number=phone_number,
+                                           city=city, zip_code=zip_code, pick_up_date=pick_up_date,
+                                           pick_up_time=pick_up_time,
+                                           pick_up_comment=pick_up_comment, user=user)
+        donation.categories.set(categories)
+        donation.save()
+        return render(request, 'first-page')
 
 class LoginView(View):
     """Login Users"""
